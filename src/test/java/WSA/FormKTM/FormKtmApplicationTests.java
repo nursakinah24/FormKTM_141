58cd1968115a6f56b735c98e4fd1b0086c9cfb19
Nur Sakinah <nursakinah2402@gmail.com>
2022-11-12 14:04:54 +0700

    first commit

---
+++ src/test/java/WSA/FormKTM/FormKtmApplicationTests.java
@@ -0,0 +1,13 @@
+package WSA.FormKTM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormKtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
