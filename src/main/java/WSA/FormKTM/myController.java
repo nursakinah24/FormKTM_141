86aa3d76252555788e4f584fc358066129130860
Nur Sakinah <nursakinah2402@gmail.com>
2022-11-12 15:16:27 +0700

    edit controller

--- src/main/java/WSA/FormKTM/myController.java
+++ src/main/java/WSA/FormKTM/myController.java
@@ -4,10 +4,37 @@
  */
 package WSA.FormKTM;
 
+import java.io.IOException;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author Asus
  */
+@Controller
 public class myController {
-    
+      @RequestMapping("/myPage")
+    public String inputData( @RequestParam(value = "varName") String nameInput,
+            @RequestParam(value = "varNIM") String nimInput,
+            @RequestParam(value = "varProdi") String prodiInput,
+            @RequestParam(value = "varEmail") String emailInput,
+            @RequestParam(value = "varImage") MultipartFile imgInput,
+            Model input
+    ) throws IOException{ byte[] img = imgInput.getBytes();
+        String encodeImage = Base64.encodeBase64String(img);
+        String imageInput = "data:image/png;base64,".concat(encodeImage);
+        
+        input.addAttribute("name", nameInput);
+        input.addAttribute("nim", nimInput);
+        input.addAttribute("prodi", prodiInput);
+        input.addAttribute("email", emailInput);
+        input.addAttribute("img", imageInput);
+        
+        return "viewpage";
+    }
 }
