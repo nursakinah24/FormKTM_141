73fbb7346425e57c7450aeee039d200111b82880
Nur Sakinah <nursakinah2402@gmail.com>
2022-11-15 18:02:24 +0700

    add date controller

--- src/main/java/WSA/FormKTM/myController.java
+++ src/main/java/WSA/FormKTM/myController.java
@@ -5,7 +5,10 @@
 package WSA.FormKTM;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,16 +26,22 @@ public class myController {
             @RequestParam(value = "varNIM") String nimInput,
             @RequestParam(value = "varProdi") String prodiInput,
             @RequestParam(value = "varEmail") String emailInput,
+            @RequestParam(value="varDate")
+            @DateTimeFormat(pattern="yyyy-MM-dd") Date date,
             @RequestParam(value = "varImage") MultipartFile imgInput,
             Model input
     ) throws IOException{ byte[] img = imgInput.getBytes();
         String encodeImage = Base64.encodeBase64String(img);
         String imageInput = "data:image/png;base64,".concat(encodeImage);
         
+        SimpleDateFormat newDate = new SimpleDateFormat("dd-MM-yyyy");
+        String dateInput = newDate.format(date);
+        
         input.addAttribute("name", nameInput);
         input.addAttribute("nim", nimInput);
         input.addAttribute("prodi", prodiInput);
         input.addAttribute("email", emailInput);
+        input.addAttribute("date", dateInput);
         input.addAttribute("img", imageInput);
         
         return "view";
